d6714c996cce10e1f5625a61d6c6021a04eaef4e
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-11-10 22:29:51 +0530

    backend init

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import errorHandler from "./middlewares/errorHandler.js";
+const auth = require("./routes/auth");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/auth", auth);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
